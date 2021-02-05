544b9a262b137089c2e6f76ae67b36e6dd03e44d
Kha Nguyen <knguyen93@myseneca.ca>
2021-02-05 15:33:28 -0500

    init server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+PORT = 3000;
+app.listen(PORT, () =>{
+    console.log(`we are connecting to PORT: ${PORT}`);
+});
