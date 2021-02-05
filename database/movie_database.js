e20d89669aca6d43bc327109d8231b665ab20c57
Kha Nguyen <knguyen93@myseneca.ca>
2021-02-05 16:50:37 -0500

    add main

---
+++ database/movie_database.js
@@ -0,0 +1,42 @@
+const movies = [
+    {
+        id : 1398601,
+        title : 'Trolls World Tour',
+        year : 2020,
+        rating: 4, // stars
+        description: "Poppy and Branch discover that they are but one of six different Troll tribes scattered over six different lands, each devoted to a different form of music: Pop, Funk, Classical, Techno, Country and Rock. Their world is about to get a lot bigger, and a whole lot louder. A member of hard-rock royalty, Queen Barb, wants to destroy all other kinds of music to let rock reign supreme. Poppy, Branch and their friends set out to visit all the other lands to unify the Trolls in harmony.",
+        category : "movie",
+        rent : 3.99,
+        buy : 14.00,
+        posterImage : "https://images2.vudu.com/poster2/1398601-168",
+        backgroundImage : "https://images2.vudu.com/background/1398601-1280a.jpg"
+
+    },
+    {
+        id : 12625,
+        title : 'Groundhog Day',
+        year : 1993,
+        rating: 4.5, // stars
+        description: "A weatherman is trapped in a personal time warp on the worst day of his life, Groundhog Day. Can he make the changes he needs to in order to break the cycle - and find true love?",
+        category : "movie",
+        rent : 2.99,
+        buy : 9.99,
+        posterImage : "https://images2.vudu.com/poster2/12625-168",
+        backgroundImage : "https://images2.vudu.com/background/12625-1280a.jpg"
+
+    },
+    {
+        id : 1592934,
+        title : 'Resident Alien: Season 1',
+        year : 1993,
+        rating: 4.5, // stars
+        description: "A weatherman is trapped in a personal time warp on the worst day of his life, Groundhog Day. Can he make the changes he needs to in order to break the cycle - and find true love?",
+        category : "tv",
+        rent : 2.99,
+        buy : 9.99,
+        posterImage : "https://images2.vudu.com/poster2/1592934-168",
+        backgroundImage : ""
+
+    },
+
+]
