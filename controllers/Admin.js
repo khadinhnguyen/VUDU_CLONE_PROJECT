0889a2ed55df7f6fa95a1dcc9664bf697f9952d0
khadinhnguyen <nguyendkha@gmail.com>
2021-03-30 22:45:52 -0400

    update validation with movie add module

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const movieModel = require('../model/Movie');
 const path = require('path');
 
+const movieModel = require('../model/Movie');
 const validation = require('../model/validation.js');
-
+const movie_ultil = require('../model/movie_ultility');
+const { movieAddValidation} = require('../model/validation')
 
 router.get('/addMovieTv', (req,res) => {
     res.render("./admin/adminMovieAddForm", {
         pageTitle : "Add Movie or TV",
     });
 });
+
+router.get('/dashboard', (req,res)=>{
+    res.render("./admin/adminDashboard", {
+        pageTitle : "Admin Dashboard"
+    })
+});
+
+router.get('/allMovieTv', (req,res)=>{
+    res.render('./admin/allMovieTvList',{
+        pageTitle : "All Movie and TV (Admin Only)",
+        movies : movie_ultil.getAllMovies()
+    })
+})
 
 router.post('/addMovieTv', (req,res)=>{
     const newMovie = {
@@ -24,34 +38,57 @@ router.post('/addMovieTv', (req,res)=>{
         feature: req.body.feature
     }
 
-    const movie = new movieModel(newMovie);
-    movie.save()
-    .then((movie)=>{     
-        console.log(req.files.smallPosterImg.name); 
-        req.files.smallPosterImg.name = `small_poster_pic_${movie._id}${path.parse(req.files.smallPosterImg.name).ext}`;
-        req.files.largePosterImg.name = `large_poster_pic_${movie._id}${path.parse(req.files.smallPosterImg.name).ext}`;
-        req.files.smallPosterImg.mv(`public/img/movie/${req.files.smallPosterImg.name}`)        
-        .then(()=>{
-            console.log("after smallPoster mv")
-            req.files.largePosterImg.mv(`public/img/movie/${req.files.largePosterImg.name}`)
-            .then(()=>{
-                movieModel.updateOne({_id:movie._id},{
-                    smallPosterImg:req.files.smallPosterImg.name,
-                    largePosterImg:req.files.largePosterImg.name
-                })
-                .then(()=>{
-                    console.log('movie is successfully save');
-                    res.redirect('/admin/addMovieTV'); 
-                })
-                .catch(err=>console.log(`Err while update poster Img ${err}`));
-            })
-            .catch(err=>console.log(`Error while inserting large poster img${err}`));                        
-        })
-        .catch(err=>console.log(`Err while mv small poster Img ${err}`));
+    const imgUpload = {
+        smallPosterImg: "",
+        largePosterImg: "",
+    };
+    if(req.files){
+        if(req.files.smallPosterImg){
+            console.log(path.parse(req.files.smallPosterImg.name).ext)
+            imgUpload.smallPosterImg = path.parse(req.files.smallPosterImg.name).ext
+        }
+        if(req.files.largePosterImg){
+            console.log(path.parse(req.files.largePosterImg.name).ext)
+            imgUpload.largePosterImg = path.parse(req.files.largePosterImg.name).ext
+        }        
+    }
+    
+    const errors = movieAddValidation(newMovie,imgUpload);
+    if(errors.errorOccured){
+        res.render("./admin/adminMovieAddForm", {
+            pageTitle : "Add Movie or TV",
+            errors
+        });
+    }
+
+    // const movie = new movieModel(newMovie);
+    // movie.save()
+    // .then((movie)=>{     
+    //     console.log(req.files.smallPosterImg.name); 
+    //     req.files.smallPosterImg.name = `small_poster_pic_${movie._id}${path.parse(req.files.smallPosterImg.name).ext}`;
+    //     req.files.largePosterImg.name = `large_poster_pic_${movie._id}${path.parse(req.files.smallPosterImg.name).ext}`;
+    //     req.files.smallPosterImg.mv(`public/img/movie/${req.files.smallPosterImg.name}`)        
+    //     .then(()=>{
+    //         console.log("after smallPoster mv")
+    //         req.files.largePosterImg.mv(`public/img/movie/${req.files.largePosterImg.name}`)
+    //         .then(()=>{
+    //             movieModel.updateOne({_id:movie._id},{
+    //                 smallPosterImg:req.files.smallPosterImg.name,
+    //                 largePosterImg:req.files.largePosterImg.name
+    //             })
+    //             .then(()=>{
+    //                 console.log('movie is successfully save');
+    //                 res.redirect('/admin/addMovieTV'); 
+    //             })
+    //             .catch(err=>console.log(`Err while update poster Img ${err}`));
+    //         })
+    //         .catch(err=>console.log(`Error while inserting large poster img${err}`));                        
+    //     })
+    //     .catch(err=>console.log(`Err while mv small poster Img ${err}`));
 
         
-    })
-    .catch(err=>console.log(`Error while inserting movie the data ${err}`));
+    // })
+    // .catch(err=>console.log(`Error while inserting movie the data ${err}`));
 
 });
 
--- model/validation.js
+++ model/validation.js
@@ -1,54 +1,95 @@
-const validation = {
+exports.registerValidation=(userInput)=> {
+    var errors = {
+        errorOccured : false,
+    };
 
-    registerValidation(userInput) {
-        var errors = {
-            errorOccured : false,
-        };
-
-        if (userInput.firstName.length < 2 || userInput.firstName.length > 30){
-            errors.errorOccured = true;
-            errors.firstNameError = "* First name must be between 2 and 30 characters long";
-        }
-
-        if (userInput.lastName.length < 2 || userInput.lastName.length > 30){
-            errors.errorOccured = true;
-            errors.lastNameError = "* Last name must be between 2 and 30 characters long";
-        }
-
-        if (userInput.email.length === 0){
-            errors.errorOccured = true;
-            errors.emailError = "* Please enter your email address";
-        }
-
-        if (userInput.checker === undefined){
-            errors.errorOccured = true;
-            errors.checkerError = "* You must agree to the Terms and Policies and Privacy Policy";
-        }
-
-        const regularExpress = /[0-9!@#$%^&*]/;
-        if (userInput.password.length < 6 || userInput.password.length > 12 || !regularExpress.test(userInput.password)){
-            errors.errorOccured = true;
-            errors.passwordError = "* Password must be between 6 and 12 characters long with at least one number or one special character !@#$%^&* ";
-        }
-
-        return errors;
-    },
-
-    loginValidation(userInput){
-        var errors = {
-            errorOccured : false,
-        };
-
-        if (userInput.userName.length === 0){
-            errors.errorOccured = true;
-            errors.userError = "* Please enter your user name";
-        }
-        if (userInput.password.length === 0){
-            errors.errorOccured = true;
-            errors.passwordError = "* Please enter your password";
-        }
-        return errors;
+    if (userInput.firstName.length < 2 || userInput.firstName.length > 30){
+        errors.errorOccured = true;
+        errors.firstNameError = "* First name must be between 2 and 30 characters long";
     }
-}
 
-module.exports = validation;
+    if (userInput.lastName.length < 2 || userInput.lastName.length > 30){
+        errors.errorOccured = true;
+        errors.lastNameError = "* Last name must be between 2 and 30 characters long";
+    }
+
+    if (userInput.email.length === 0){
+        errors.errorOccured = true;
+        errors.emailError = "* Please enter your email address";
+    }
+
+    if (userInput.checker === undefined){
+        errors.errorOccured = true;
+        errors.checkerError = "* You must agree to the Terms and Policies and Privacy Policy";
+    }
+
+    const regularExpress = /[0-9!@#$%^&*]/;
+    if (userInput.password.length < 6 || userInput.password.length > 12 || !regularExpress.test(userInput.password)){
+        errors.errorOccured = true;
+        errors.passwordError = "* Password must be between 6 and 12 characters long with at least one number or one special character !@#$%^&* ";
+    }
+
+    return errors;
+},
+
+exports.loginValidation=(userInput)=>{
+    var errors = {
+        errorOccured : false,
+    };
+
+    if (userInput.userName.length === 0){
+        errors.errorOccured = true;
+        errors.userError = "* Please enter your user name";
+    }
+    if (userInput.password.length === 0){
+        errors.errorOccured = true;
+        errors.passwordError = "* Please enter your password";
+    }
+    return errors;
+};
+
+exports.movieAddValidation=(userInput,images)=> {
+    var errors = {
+        errorOccured : false,
+    };
+
+    if (userInput.title.length < 1){
+        errors.errorOccured = true;
+        errors.titleError = "* Please enter title";
+    }
+
+    if (userInput.synopsis.length < 1){
+        errors.errorOccured = true;
+        errors.synopsisError = "* Please provide synopsis";
+    }
+
+    if (userInput.rentalPrice <= 0){
+        errors.errorOccured = true;
+        errors.rentalPriceError = "* Please enter valid Rental Price";
+    }
+
+    if (userInput.purchasePrice <=0){
+        errors.errorOccured = true;
+        errors.purchasePriceError = "* Please enter valid Purchase Price";
+    }
+
+    if (images.smallPosterImg.length == 0){
+        errors.errorOccured = true;
+        errors.smallPosterImgError = "* Please upload Small Poster Image";
+    }
+    if (images.largePosterImg.length == 0){
+        errors.errorOccured = true;
+        errors.largePosterImgError = "* Please upload Large Poster Image";
+    }
+    const regularExpress = /.jpg|.gifs|.pngs/;
+    if (images.smallPosterImg.length > 0 && !regularExpress.test(images.smallPosterImg)){
+        errors.errorOccured = true;
+        errors.smallPosterImgError = "* Please upload image file .jpgs / .gifs / .pngs";
+    }
+    if (images.largePosterImg.length > 0 && !regularExpress.test(images.largePosterImg)){
+        errors.errorOccured = true;
+        errors.largePosterImgError = "* Please upload image file .jpgs / .gifs / .pngs";
+    }
+
+    return errors;
+}
